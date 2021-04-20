0de1b745f1a0ae1858324efc626da1294a14f232
Gustavo-12 <gustavo.olenscki@gmail.com>
2021-04-19 23:00:56 -0400

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
 
-export default function Courses() {
-  return(
-    <div className="wrapper">
+class Courses extends Component {
+  state = {};
+
+  handleAdd = () => {};
+
+  render() {
+    return (
+      <div className="wrapper">
         <h2>Courses</h2>
-        <h3>    In this page a student will be able to see all the courses that they are currently enrolled as well as past enrollments.</h3>
-    </div>
-  );
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Course Name</th>
+              <th scope="col">Overall Grade</th>
+              <th scope="col">Instructor Name</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>
+                <button
+                  onClick={() => this.handleAdd()}
+                  className="btn btn-danger"
+                >
+                  Add Course
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+export default Courses;
