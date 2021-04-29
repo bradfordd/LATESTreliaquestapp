88c4398cc0b1fef00b89970615bf474aa36d1801
Gustavo-12 <gustavo.olenscki@gmail.com>
2021-04-29 18:12:18 -0400

    Admin more

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -6,228 +6,235 @@
 	"student": ["jeffrey", "tom", "Beth"]
 }
 */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-let Course = require('../models/coursemodel');
+let Course = require("../models/coursemodel");
 const Register = require("../models/registermodel");
-let Grade = require('../models/grademodel');
+let Grade = require("../models/grademodel");
 
 //posts a new course, requires name, teacherassigned, and teacherID
-router.route('/').post(async(req, res) => {
-    const name = req.body.name;
-    const teacherAssigned = req.body.teacherAssigned;
-    const teacherID = req.body.teacherID;
-    const students = [];
-   
-    const newCourse = new Course({name, teacherAssigned, teacherID, students});
-    newCourse.save();
-    const newCourseID = newCourse._id;
-     Register.updateOne(
-      { _id: teacherID },
-      { $push: { assignedCoursesIDs: newCourseID } })
-    .then(res.json("Course Added!"))   
-   .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").post(async (req, res) => {
+  const name = req.body.name;
+  const teacherAssigned = req.body.teacherAssigned;
+  const teacherID = req.body.teacherID;
+  const students = [];
+
+  const newCourse = new Course({ name, teacherAssigned, teacherID, students });
+  newCourse.save();
+  const newCourseID = newCourse._id;
+  Register.updateOne(
+    { _id: teacherID },
+    { $push: { assignedCoursesIDs: newCourseID } }
+  )
+    .then(res.json("Course Added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 //Assigns a teacher to a course
 //Requires a teacherID and courseID
-router.route('/assignTeacher').post(async(req, res) => {
+router.route("/assignTeacher").post(async (req, res) => {
   var courseID = req.body.courseID;
-  var courseID = await Course.find({_id: courseID});
+  var courseID = await Course.find({ _id: courseID });
   courseID = courseID[0]._id;
   const teacherID = req.body.teacherID;
-  const teacherInfo = await Register.find({_id: teacherID});
+  const teacherInfo = await Register.find({ _id: teacherID });
   const teacherName = teacherInfo[0].name;
-  Course.updateOne( {_id: courseID}, {teacherAssigned: teacherName});
+  Course.updateOne({ _id: courseID }, { teacherAssigned: teacherName });
   Register.updateOne(
     { _id: teacherID },
-    { $push: { assignedCoursesIDs: courseID } })
-  .then(res.json("Course Added!"))   
- .catch(err => res.status(400).json('Error: ' + err));
+    { $push: { assignedCoursesIDs: courseID } }
+  )
+    .then(res.json("Course Added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 //Gets ALL courses
-router.route('/allcourses').get(async(req,res) => {
-   //const courseID = req.body.courseID;
-   Course.find()
-     .then(course => res.json(course))
-     .catch(err => res.status(400).json('Error: ' + err));
+router.route("/allcourses").get(async (req, res) => {
+  //const courseID = req.body.courseID;
+  Course.find()
+    .then(course => res.json(course))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 //returns all the courses a student is currently enrolled in
 //requires studentID
-router.route('/studentcourses').post(async(req,res) => {
-   const studentID = req.body.studentID;
-   var student = await Register.find({_id : studentID});
-   var assignedCoursesIDs = student[0].assignedCoursesIDs;
-   var courseInformation = [];
-   for (var i = 0; i < assignedCoursesIDs.length; i++) {
-    var tempInformation = await Course.find({_id : assignedCoursesIDs[i]});
-    courseInformation.push(tempInformation);  
-   }
-   res.json(courseInformation);
-     //.then(course => res.json(course))
-     //.catch(err => res.status(400).json('Error: ' + err));
+router.route("/studentcourses").post(async (req, res) => {
+  const studentID = req.body.studentID;
+  var student = await Register.find({ _id: studentID });
+  var assignedCoursesIDs = student[0].assignedCoursesIDs;
+  var courseInformation = [];
+  for (var i = 0; i < assignedCoursesIDs.length; i++) {
+    var tempInformation = await Course.find({ _id: assignedCoursesIDs[i] });
+    courseInformation.push(tempInformation);
+  }
+  res.json(courseInformation);
+  //.then(course => res.json(course))
+  //.catch(err => res.status(400).json('Error: ' + err));
 });
 //returns list of course names
 //requires array of course IDs
-router.route('/names').get(async(req,res) => {
-   var names = [];
-   var courses = [];
-   for (var i = 0; i < req.body.courses.length; i++) {
-     courses.push(req.body.courses[i]);
-   }
-   for (var i = 0; i < courses.length; i++) {
-     var temp = await Course.find({_id: courses[i]});
-     var newName = temp[0].name;
-     names.push(newName);
-   }
-   res.json(names);
- });
+router.route("/names").get(async (req, res) => {
+  var names = [];
+  var courses = [];
+  for (var i = 0; i < req.body.courses.length; i++) {
+    courses.push(req.body.courses[i]);
+  }
+  for (var i = 0; i < courses.length; i++) {
+    var temp = await Course.find({ _id: courses[i] });
+    var newName = temp[0].name;
+    names.push(newName);
+  }
+  res.json(names);
+});
 
- //returns names of instructors given courseIDs
- router.route('/instructorNames').get(async(req,res) => {
-   var names = [];
-   var courses = [];
-   for (var i = 0; i < req.body.courses.length; i++) {
-     courses.push(req.body.courses[i]);
-   }
-   for (var i = 0; i < courses.length; i++) {
-     var temp = await Course.find({_id: courses[i]});
-     var newName = temp[0].teacherAssigned;
-     names.push(newName);
-   }
-   res.json(names);
- });
+//returns names of instructors given courseIDs
+router.route("/instructorNames").get(async (req, res) => {
+  var names = [];
+  var courses = [];
+  for (var i = 0; i < req.body.courses.length; i++) {
+    courses.push(req.body.courses[i]);
+  }
+  for (var i = 0; i < courses.length; i++) {
+    var temp = await Course.find({ _id: courses[i] });
+    var newName = temp[0].teacherAssigned;
+    names.push(newName);
+  }
+  res.json(names);
+});
 //returns information about a course
 //requires course ID
-router.route('/').get((req, res) => {
-   const courseID = req.body.courseID;
-   Course.findOne({ _id: courseID})
-     .then(course => res.json(course))
-     .catch(err => res.status(400).json('Error: ' + err));
+router.route("/showcourseinfo").post((req, res) => {
+  const courseID = req.body.courseID;
+  Course.findOne({ _id: courseID })
+    .then(course => res.json(course))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 //This is for the case in a Teacher can't be assigned at the time of creation
-router.route('/noTeacher').post((req, res) => {
-   const name = req.body.name;
-   const teacherAssigned = "No Teacher Assigned";
-   const students = [];
-   //for (i = 0; i < req.body.students.length; i++) {
-   //    students.push(req.body.students[i])
-   //}
+router.route("/noTeacher").post((req, res) => {
+  const name = req.body.name;
+  const teacherAssigned = "No Teacher Assigned";
+  const students = [];
+  //for (i = 0; i < req.body.students.length; i++) {
+  //    students.push(req.body.students[i])
+  //}
 
-   const newCourse = new Course({name, teacherAssigned, students});
- 
-   newCourse.save()
-     .then(() => res.json('Course added!'))
-     .catch(err => res.status(400).json('Error: ' + err));
+  const newCourse = new Course({ name, teacherAssigned, students });
+
+  newCourse
+    .save()
+    .then(() => res.json("Course added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 //returns array of gradeAverages corresponding to course grades
 //requires studentID and array of courseIDs
-router.route('/gradeAverages').get(async(req,res) => {
+router.route("/gradeAverages").get(async (req, res) => {
   const courseIDArray = req.body.courseIDs;
   const studentID = req.body.studentID;
   var coursesGrades = [];
-  for (var k = 0; k < courseIDArray.length; k++)
-  {
-  var grades = await Grade.find( { courseID: courseIDArray[k], studentID: studentID});
-  var assignedGradesTotal = 0;
-  var gradesTotal = 0;
-  for (var i = 0; i < grades.length; i++) {
-    assignedGradesTotal += grades[i].gradeAssigned;
-    gradesTotal += grades[i].total;
+  for (var k = 0; k < courseIDArray.length; k++) {
+    var grades = await Grade.find({
+      courseID: courseIDArray[k],
+      studentID: studentID,
+    });
+    var assignedGradesTotal = 0;
+    var gradesTotal = 0;
+    for (var i = 0; i < grades.length; i++) {
+      assignedGradesTotal += grades[i].gradeAssigned;
+      gradesTotal += grades[i].total;
+    }
+    numberGrade = assignedGradesTotal / gradesTotal;
+    numberGrade = numberGrade * 100;
+    coursesGrades.push(numberGrade);
   }
-  numberGrade = assignedGradesTotal / gradesTotal;
-  numberGrade = numberGrade * 100;
-  coursesGrades.push(numberGrade);
-   }
   res.json(coursesGrades);
 });
 
 //Requires courseID
-router.route('/gradeAveragesCourse').post(async(req,res) => {
+router.route("/gradeAveragesCourse").post(async (req, res) => {
   const courseID = req.body.courseID;
-  
-  const courseInfo = await Course.find({_id: courseID});
+
+  const courseInfo = await Course.find({ _id: courseID });
   const students = courseInfo[0].students;
   //res.json(students);
   //var coursesGrades = [];
   for (var m = 0; m < students.length; m++) {
     var studentID = students[m];
-    var studentInfo = await Register.find({_id : studentID});
+    var studentInfo = await Register.find({ _id: studentID });
     //var studentInfo = await Register.findById(studentID);
     res.json(studentInfo);
     const studentName = studentInfo.name;
     const courseIDArray = studentInfo.assignedCoursesIDs;
     //res.json(studentName);
-  for (var k = 0; k < courseIDArray.length; k++)
-  {
-  var grades = await Grade.find( { courseID: courseIDArray[k], studentID: studentID});
-  var assignedGradesTotal = 0;
-  var gradesTotal = 0;
-  for (var i = 0; i < grades.length; i++) {
-    assignedGradesTotal += grades[i].gradeAssigned;
-    gradesTotal += grades[i].total;
+    for (var k = 0; k < courseIDArray.length; k++) {
+      var grades = await Grade.find({
+        courseID: courseIDArray[k],
+        studentID: studentID,
+      });
+      var assignedGradesTotal = 0;
+      var gradesTotal = 0;
+      for (var i = 0; i < grades.length; i++) {
+        assignedGradesTotal += grades[i].gradeAssigned;
+        gradesTotal += grades[i].total;
+      }
+      numberGrade = assignedGradesTotal / gradesTotal;
+      numberGrade = numberGrade * 100;
+      coursesGrades.push(numberGrade);
+    }
   }
-  numberGrade = assignedGradesTotal / gradesTotal;
-  numberGrade = numberGrade * 100;
-  coursesGrades.push(numberGrade);
-  }
-}
   res.json(coursesGrades);
 });
 
 //gets all courses a student is NOT currently signed up for
 //requires studentID
-router.route('/allCoursesStudentDoesntHave').post(async(req, res) => {
-   const studentID = req.body.studentID;
-   var student = await Register.find( { _id: studentID});
-   const studentAssignedCourses = student[0].assignedCoursesIDs;
-   //res.json(studentAssignedCourses);
-   const allCourses = await Course.find();
-   for (var i = 0; i < studentAssignedCourses.length; i++) {
-     for (var j = 0; j < allCourses.length; j++) {
+router.route("/allCoursesStudentDoesntHave").post(async (req, res) => {
+  const studentID = req.body.studentID;
+  var student = await Register.find({ _id: studentID });
+  const studentAssignedCourses = student[0].assignedCoursesIDs;
+  //res.json(studentAssignedCourses);
+  const allCourses = await Course.find();
+  for (var i = 0; i < studentAssignedCourses.length; i++) {
+    for (var j = 0; j < allCourses.length; j++) {
       if (allCourses[j]._id == studentAssignedCourses[i])
         allCourses.splice(j, 1);
-     }
-   }
-   res.json(allCourses);
+    }
+  }
+  res.json(allCourses);
 });
 
 //returns all the courses a teacher is currently enrolled in
 //requires teacherid
-router.route('/teachercourses').post(async(req,res) => {
+router.route("/teachercourses").post(async (req, res) => {
   const teacherID = req.body.teacherID;
-  var teacher = await Register.find({_id : teacherID});
+  var teacher = await Register.find({ _id: teacherID });
   var assignedCoursesIDs = teacher[0].assignedCoursesIDs;
   //res.json(assignedCoursesIDs);
   var courseInformation = [];
   for (var i = 0; i < assignedCoursesIDs.length; i++) {
-   var tempInformation = await Course.find({_id : assignedCoursesIDs[i]});
-   //res.json(tempInformation);
-   courseInformation.push(tempInformation);  
+    var tempInformation = await Course.find({ _id: assignedCoursesIDs[i] });
+    //res.json(tempInformation);
+    courseInformation.push(tempInformation);
   }
   //courseInformation.shift();
   res.json(courseInformation);
-    //.then(course => res.json(course))
-    //.catch(err => res.status(400).json('Error: ' + err));
+  //.then(course => res.json(course))
+  //.catch(err => res.status(400).json('Error: ' + err));
 });
 
 //returns all students enrolled in a course
 //requires courseID
-router.route('/studentsInCourse').post(async(req,res) => {
+router.route("/studentsInCourse").post(async (req, res) => {
   const courseID = req.body.courseID;
-  var courses = await Course.find({_id : courseID});
+  var courses = await Course.find({ _id: courseID });
   var students = courses[0].students;
   var studentInfo = [];
   for (var i = 0; i < students.length; i++) {
-   var tempInformation = await Register.find({_id : students[i]});
-   studentInfo.push(tempInformation);  
+    var tempInformation = await Register.find({ _id: students[i] });
+    studentInfo.push(tempInformation);
   }
   res.json(studentInfo);
-    //.then(course => res.json(course))
-    //.catch(err => res.status(400).json('Error: ' + err));
+  //.then(course => res.json(course))
+  //.catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('')
+router.route("");
 module.exports = router;
--- src/components/AdminFolder/EnglishAdmin.js
+++ src/components/AdminFolder/EnglishAdmin.js
@@ -1,14 +1,132 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function Personal_info() {
-  return (
-    <div className="wrapper">
-      <h2>Personal Information</h2>
-      <h3>
-        {" "}
-        In this page a student will be able to see all their personal records,
-        such address, phone number, etc.
-      </h3>
-    </div>
-  );
+const Course = props => (
+  <tr>
+    <td>{props.course.studentName}</td>
+    <td>{props.course.grade}</td>
+    <td>
+      <button
+        onClick={() =>
+          props.updateGrade(props.course.studentID, props.course.grade)
+        }
+        className="btn btn-danger"
+      >
+        Remove User From The Database
+      </button>
+    </td>
+  </tr>
+);
+
+export default class English extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateGrade = this.updateGrade.bind(this);
+    //this.showTeachers = this.showTeachers.bind(this);
+
+    this.state = { courses: [], all_courses: [] };
+  }
+
+  componentDidMount() {
+    localStorage.removeItem("targetID");
+    localStorage.removeItem("grade");
+    var courseID = localStorage.getItem("courseID");
+    var tempStudentID = "";
+    var anotherTempStudentID = "";
+
+    tempStudentID = courseID.replace('"', "");
+    anotherTempStudentID = tempStudentID.replace('"', "");
+    courseID = anotherTempStudentID;
+    var body = {
+      courseID: courseID,
+    };
+    console.log(courseID);
+    axios
+      .put("http://localhost:8080/components/gradeaverage/entireCourse", body)
+      .then(response => {
+        this.setState({ courses: response.data });
+        console.log(this.state.courses);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    var body2 = {
+      courseID: courseID,
+    };
+
+    axios
+      .post("http://localhost:8080/components/course/showcourseinfo", body2)
+      .then(response => {
+        this.setState({ all_courses: response.data });
+        console.log(this.state.all_courses);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /* showTeacher(teacher_id) {
+    return this.state.all_courses
+  }*/
+
+  updateGrade(id, grade) {
+    console.log(id);
+    localStorage.setItem("targetID", JSON.stringify(id));
+    //localStorage.setItem("grade", JSON.stringify(grade));
+
+    window.location = "/components/allcourses/updategradeform";
+  }
+
+  courseList() {
+    return this.state.courses.map(currentcourse => {
+      return (
+        <Course
+          course={currentcourse}
+          updateGrade={this.updateGrade}
+          key={currentcourse._id}
+        />
+      );
+    });
+  }
+
+  render() {
+    if (this.state.courses.length === 0)
+      return <p>No students assgned to this course.</p>;
+
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <h2>Information About English</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Instructor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {" "}
+              <tr>
+                <td>{this.state.all_courses.teacherAssigned}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="wrapper">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Student Name</th>
+                <th scope="col">Overall Grade</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>{this.courseList()}</tbody>
+          </table>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
